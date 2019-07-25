85461bf6fdf1fb11aac3f1c88a9635652ec0d153
28810 <28810@YEXIANGQIN>
2019-07-25 16:15:18 +0800

    工作单元

--- Domain/BaseEntityUnitOfWork.cs
+++ Domain/BaseEntityUnitOfWork.cs
@@ -1,123 +1,110 @@
-﻿using FreeSql;
-using SafeObjectPool;
+﻿using SafeObjectPool;
 using System;
 using System.Data;
 using System.Data.Common;
 using System.Threading;
 
-class BaseEntityUnitOfWork : IUnitOfWork
+namespace FreeSql
 {
-    internal readonly static AsyncLocal<IUnitOfWork> _asyncUow = new AsyncLocal<IUnitOfWork>();
-
-    protected IFreeSql _fsql;
-    protected Object<DbConnection> _conn;
-    protected DbTransaction _tran;
-
-    public BaseEntityUnitOfWork(IFreeSql fsql)
+    class UnitOfWork : IUnitOfWork
     {
-        _fsql = fsql;
-        _asyncUow.Value = this;
-    }
+//#if ns20
+        public static readonly AsyncLocal<IUnitOfWork> CurrentUow = new AsyncLocal<IUnitOfWork>();
+//#endif
 
-    void ReturnObject()
-    {
-        _fsql.Ado.MasterPool.Return(_conn);
-        _tran = null;
-        _conn = null;
-        _asyncUow.Value = null;
-    }
+        protected IFreeSql _fsql;
+        protected Object<DbConnection> _conn;
+        protected DbTransaction _tran;
 
-
-    /// <summary>
-    /// 是否启用工作单元
-    /// </summary>
-    public bool Enable { get; private set; } = true;
-
-    /// <summary>
-    /// 禁用工作单元
-    /// <exception cref="Exception"></exception>
-    /// <para></para>
-    /// 若已开启事务（已有Insert/Update/Delete操作），调用此方法将发生异常，建议在执行逻辑前调用
-    /// </summary>
-    public void Close()
-    {
-        if (_tran != null)
-            throw new Exception("已开启事务，不能禁用工作单元");
-
-        Enable = false;
-    }
-
-    public void Open() =>
-        Enable = true;
-
-    public IsolationLevel? IsolationLevel { get; set; }
-
-    public DbTransaction GetOrBeginTransaction(bool isCreate = true)
-    {
-
-        if (_tran != null) return _tran;
-        if (isCreate == false) return null;
-        if (!Enable) return null;
-        if (_conn != null) _fsql.Ado.MasterPool.Return(_conn);
-
-        _conn = _fsql.Ado.MasterPool.Get();
-        try
+        public UnitOfWork(IFreeSql fsql)
         {
-            _tran = IsolationLevel == null ?
-                _conn.Value.BeginTransaction() :
-                _conn.Value.BeginTransaction(IsolationLevel.Value);
+            _fsql = fsql;
+//#if ns20
+            CurrentUow.Value = this;
+//#endif
         }
-        catch
-        {
-            ReturnObject();
-            throw;
-        }
-        return _tran;
-    }
 
-    public void Commit()
-    {
-        if (_tran != null)
+        void ReturnObject()
+        {
+            _fsql.Ado.MasterPool.Return(_conn);
+            _tran = null;
+            _conn = null;
+//#if ns20
+            CurrentUow.Value = null;
+//#endif
+        }
+
+        public bool Enable { get; private set; } = true;
+
+        public void Close()
+        {
+            if (_tran != null)
+                throw new Exception("已开启事务，不能禁用工作单元");
+
+            Enable = false;
+        }
+        public void Open()
+        {
+            Enable = true;
+        }
+
+        public IsolationLevel? IsolationLevel { get; set; }
+
+        public DbTransaction GetOrBeginTransaction(bool isCreate = true)
+        {
+            if (_tran != null) return _tran;
+            if (isCreate == false) return null;
+            if (!Enable) return null;
+            if (_conn != null) _fsql.Ado.MasterPool.Return(_conn);
+
+            _conn = _fsql.Ado.MasterPool.Get();
+            try
+            {
+                _tran = IsolationLevel == null ?
+                    _conn.Value.BeginTransaction() :
+                    _conn.Value.BeginTransaction(IsolationLevel.Value);
+            }
+            catch
+            {
+                ReturnObject();
+                throw;
+            }
+            return _tran;
+        }
+
+        public void Commit()
         {
             try
             {
-                _tran.Commit();
+                if (_tran != null) _tran.Commit();
             }
             finally
             {
                 ReturnObject();
             }
         }
-    }
-    public void Rollback()
-    {
-        if (_tran != null)
+        public void Rollback()
         {
             try
             {
-                _tran.Rollback();
+                if (_tran != null) _tran.Rollback();
             }
             finally
             {
                 ReturnObject();
             }
         }
-    }
-    ~BaseEntityUnitOfWork()
-    {
-        this.Dispose();
-    }
-    bool _isdisposed = false;
-    public void Dispose()
-    {
-        if (_isdisposed) return;
-        _isdisposed = true;
-        try
+        ~UnitOfWork()
         {
+            this.Dispose();
+        }
+        bool _isdisposed = false;
+        public void Dispose()
+        {
+            if (_isdisposed) return;
+            _isdisposed = true;
             this.Rollback();
-        }
-        finally
-        {
+            this.Close();
             GC.SuppressFinalize(this);
         }
     }
